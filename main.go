316475104d41211dbcd8b9cd7c1bfb3181cd42d1
faizZen <Faizm@zenithive.com>
2025-03-12 09:50:10 +0530

    Update main.go

--- main.go
+++ main.go
@@ -10,10 +10,6 @@ import (
 
 func init() {
 	initializers.ConnectToDatabase()
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 }
 func main() {
 	graphql.Handler()
