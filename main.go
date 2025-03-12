5e5296e1c0995e3005a08ce08b29948191367d90
faizZen <Faizm@zenithive.com>
2025-03-12 09:51:25 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"log"
+
 
 	initializers "github.com/Zenithive/it-crm-backend/Initializers"
 	"github.com/Zenithive/it-crm-backend/internal/graphql"
-	"github.com/joho/godotenv"
 )
 
 func init() {
