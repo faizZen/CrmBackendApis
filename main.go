0bcf98e7b74109bca4b7d66d3499596eb75c126b
faizZen <Faizm@zenithive.com>
2025-03-13 10:41:44 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,20 +1,14 @@
 package main
 
 import (
-	"log"
-
 	initializers "github.com/Zenithive/it-crm-backend/Initializers"
 	"github.com/Zenithive/it-crm-backend/auth"
 	"github.com/Zenithive/it-crm-backend/internal/graphql"
-	"github.com/joho/godotenv"
 )
 
 func init() {
 	initializers.ConnectToDatabase()
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+
 	auth.InitGoogleStore()
 }
 func main() {
